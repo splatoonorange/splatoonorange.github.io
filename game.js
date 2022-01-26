f82bbc693d14a8bb89e87596abaec2ae4f3bb544
Ryan <lucidorangee@gmail.com>
2022-01-25 23:12:34 -0500

    change image for every click

--- game.js
+++ game.js
@@ -1,26 +1,37 @@
 /**
  * Created by lucidorangee / 오렝 (유튜브)
  */
+
+ c=document.getElementById('canv').getContext("2d")
+ index=1;
+ var data;
+ readTextFile("weaponinfo.json", function(text){
+    var data = JSON.parse(text); //parse JSON
+});
+
  window.onload = function() {
-    c=document.getElementById('canv').getContext("2d")
     c.fillStyle="#000000"
     c.font="60px monospace"
-    var img = document.getElementById("52_Gal_Deco");
-    c.drawImage(img, 512, 0)
     
 
     //Draw rectangles for subs
     for(let i = 0; i < 13; i++)
         c.fillRect(i*95+28,582,85,85)
 
-    readTextFile("weaponinfo.json", function(text){
-        var data = JSON.parse(text); //parse JSON
-        console.log(data.main[0].imagename.replace(".png",""));
-    });
+    
+    var imgname = data.main[index++].imagename.replace(".png","");
+    var img = document.getElementById(imgname);
+    c.drawImage(img, 512, 0)
+    
  }
 
  window.addEventListener("click", function(event) {
     console.log(event.clientX);
+
+    var imgname = data.main[index++].imagename.replace(".png","");
+    var img = document.getElementById(imgname);
+    c.drawImage(img, 512, 0)
+
 
  });
 

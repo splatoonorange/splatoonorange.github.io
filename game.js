28c713cebbce3135940ae18583e2bb61012cd400
Ryan <lucidorangee@gmail.com>
2022-01-26 00:25:24 -0500

    Added RNG

--- game.js
+++ game.js
@@ -2,22 +2,21 @@
  * Created by lucidorangee / 오렝 (유튜브)
  */
 
- c=document.getElementById('canv').getContext("2d")
- index=1;
+ var c=document.getElementById('canv').getContext("2d")
+ var index=1;
  var data;
  readTextFile("weaponinfo.json", function(text){
     data = JSON.parse(text); //parse JSON
 });
+ var weaponnum = data.main.length;
 
  window.onload = function() {
     c.fillStyle="#000000"
     c.font="60px monospace"
     
-
     //Draw rectangles for subs
     for(let i = 0; i < 13; i++)
         c.fillRect(i*95+28,582,85,85)
-
     
     var imgname = data.main[index++].imagename.replace(".png","");
     var img = document.getElementById(imgname);
@@ -29,15 +28,14 @@
     console.log(event.clientX);
 
     c.clearRect(512, 0, 256, 256);
+    index = Math.floor((Math.random() * weaponnum) + 1);
+
     var imgname = data.main[index].imagename.replace(".png","");
     var img = document.getElementById(imgname);
     c.drawImage(img, 512, 0)
     console.log(data.main[index].name);
     console.log(data.main[index].subweapon);
     console.log(data.main[index].specialweapon);
-    index++;
-
-
  });
 
  function readTextFile(file, callback) {

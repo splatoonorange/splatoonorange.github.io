63b591ecde7fc2d89ef7f295fb282bd1ee2f9431
Ryan <lucidorangee@gmail.com>
2022-01-25 22:33:25 -0500

    Test1 for JSON read

--- game.js
+++ game.js
@@ -7,16 +7,32 @@
     c.font="60px monospace"
     var img = document.getElementById("52_Gal_Deco");
     c.drawImage(img, 512, 0)
-
-    const jsonData= require('weaponinfo.json'); 
-    console.log(jsonData);
+    
 
     //Draw rectangles for subs
     for(let i = 0; i < 13; i++)
         c.fillRect(i*95+28,582,85,85)
+
+    readTextFile("weaponinfo.json", function(text){
+        var data = JSON.parse(text); //parse JSON
+        console.log(data);
+    });
  }
 
  window.addEventListener("click", function(event) {
     console.log(event.clientX);
 
  });
+
+ function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+

8bfbe2497bb0f343c843067be591b6de013234ed
Ryan <lucidorangee@gmail.com>
2022-01-26 15:08:09 -0500

    Mouse position calculation updated

--- game.js
+++ game.js
@@ -2,7 +2,8 @@
  * Created by lucidorangee / 오렝 (유튜브)
  */
 
- var c=document.getElementById('canv').getContext("2d")
+ var ctx=document.getElementById('canv').getContext("2d")
+ var canvas=document.getElementById('canv');
  var index=1;
  var data;
  var phase=0;
@@ -14,34 +15,37 @@
 
  window.onload = function() {
     
-    c.font="60px monospace";
-    c.textAlign = "center";
-    c.fillText("Click anywhere to begin", 640, 360);
+    ctx.font="60px monospace";
+    ctx.textAlign = "center";
+    ctx.fillText("Click anywhere to begin", 640, 360);
     
  }
-
- window.addEventListener("click", function(event) {
-    console.log(event.clientX);
+window.addEventListener("mousedown", function(event) {
+    var rect = canvas.getBoundingClientRect();
+    console.log("width: " + rect.right);
+    console.log("mouse at x: " + event.clientX + " y: " + event.clientY);
+    var ratio = window.innerWidth / (rect.right-rect.left);
+    console.log("Relative mouse position: " + (((event.clientX-rect.left)/window.innerWidth)*1280*ratio))   
+    var mousex = (((event.clientX-rect.left)/window.innerWidth)*1280*ratio);
 
     if(phase == 0)
     {
         //Move the game to phase 1
         phase = 1;
-        c.clearRect(0, 0, 1280, 720);
-        c.font="60px monospace";
-        c.fillStyle = "#000000";
-        c.textAlign = "left";
-        c.fillText(timer, 32, 64);
+        ctx.clearRect(0, 0, 1280, 720);
+        ctx.font="60px monospace";
+        ctx.fillStyle = "#000000";
+        ctx.textAlign = "left"; 
+        ctx.fillText(timer, 32, 64);
 
         //Timer
         var x = setInterval(function(){
-            c.font="60px monospace";
-            c.fillStyle = "#000000";
-            c.textAlign = "left";
+            ctx.font="60px monospace";
+            ctx.fillStyle = "#000000";
+            ctx.textAlign = "left";
             timer--;
-            console.log(c.measureText("Hello").height);
-            c.clearRect(32, 6, c.measureText(timer).width, 60);
-            c.fillText(timer, 32, 64);
+            ctx.clearRect(32, 6, ctx.measureText(timer).width, 60);
+            ctx.fillText(timer, 32, 64);
         }, 1000);
 
         //display weapon and subs
@@ -49,7 +53,7 @@
     
         var imgname = data.main[index].imagename.replace(".png","");
         var img = document.getElementById(imgname);
-        c.drawImage(img, 512, 0)
+        ctx.drawImage(img, 512, 0)
         showSubOptions();
 
         return;
@@ -64,7 +68,7 @@
         if(event.clientY > 420 && event.clientY < 420+142)
         {
             for(var i = 0; i < 6; i++){
-                if(event.clientX > i*158+159 && event.clientX < i*158+159+142)
+                if(mousex > i*158+159 && mousex < i*158+159+142)
                 {
                     choice = i;
                     break;
@@ -75,7 +79,7 @@
         else if(event.clientY > 580 && event.clientY < 580+142)
         {
             for(var i = 0; i < 7; i++){
-                if(event.clientX > i*158+80 && event.clientX < i*158+80+142)
+                if(mousex > i*158+80 && mousex < i*158+80+142)
                 {
                     choice = i;
                     break;
@@ -88,10 +92,10 @@
         //test purpose
         console.log("Chosen is " + data.sub[choice]);
 
-        c.textAlign = "left";
-        c.fillStyle = "#000000";
-        c.clearRect(32, 70, c.measureText("Phase " + phase).width, 60);
-        c.fillText("Phase " + phase, 32, 128);
+        ctx.textAlign = "left";
+        ctx.fillStyle = "#000000";
+        ctx.clearRect(32, 70, ctx.measureText("Phase " + phase).width, 60);
+        ctx.fillText("Phase " + phase, 32, 128);
         phase = 2;
         showSpecialOptions();
 
@@ -107,12 +111,12 @@
     if(phase == 2)
     {
         phase = 1;
-        c.clearRect(512, 0, 256, 256);
+        ctx.clearRect(512, 0, 256, 256);
         index = Math.floor((Math.random() * data.main.length) + 1);
     
         var imgname = data.main[index].imagename.replace(".png","");
         var img = document.getElementById(imgname);
-        c.drawImage(img, 512, 0)
+        ctx.drawImage(img, 512, 0)
         showSubOptions();
         console.log(data.main[index].name);
         console.log(data.main[index].subweapon);
@@ -137,46 +141,46 @@
 }
 
 function showSubOptions(){
-    c.fillStyle="#DCDCDC"
-    c.clearRect(0, 360, 1280, 360);
+    ctx.fillStyle="#DCDCDC"
+    ctx.clearRect(0, 360, 1280, 360);
     
     //Draw rectangles for subs
     for(let i = 0; i < 6; i++)
     {
-        c.fillRect(i*158+159,420,142,142)
+        ctx.fillRect(i*158+159,420,142,142)
         var imgname = data.sub[i].imagename.replace(".png","");
         var img = document.getElementById(imgname);
-        c.drawImage(img, i*158+166, 427, 128, 128)
+        ctx.drawImage(img, i*158+166, 427, 128, 128)
     }
 
     for(let i = 6; i < 13; i++)
     {
-        c.fillRect((i-6)*158+80,580,142,142)
+        ctx.fillRect((i-6)*158+80,580,142,142)
         var imgname = data.sub[i].imagename.replace(".png","");
         var img = document.getElementById(imgname);
-        c.drawImage(img, (i-6)*158+87, 587, 128, 128)
+        ctx.drawImage(img, (i-6)*158+87, 587, 128, 128)
     }
 }
 
 function showSpecialOptions(){
-    c.fillStyle="#DCDCDC"
-    c.clearRect(0, 360, 1280, 360);
+    ctx.fillStyle="#DCDCDC"
+    ctx.clearRect(0, 360, 1280, 360);
     
     //Draw rectangles for subs
     for(let i = 0; i < 8; i++)
     {
-        c.fillRect(i*158+8,420,142,142)
+        ctx.fillRect(i*158+8,420,142,142)
         var imgname = data.special[i].imagename.replace(".png","");
         var img = document.getElementById(imgname);
-        c.drawImage(img, i*158+15, 427, 128, 128)
+        ctx.drawImage(img, i*158+15, 427, 128, 128)
     }
 
     for(let i = 8; i < 16; i++)
     {
-        c.fillRect((i-8)*158+8,580,142,142)
+        ctx.fillRect((i-8)*158+8,580,142,142)
         var imgname = data.special[i].imagename.replace(".png","");
         var img = document.getElementById(imgname);
-        c.drawImage(img, (i-8)*158+15, 587, 128, 128)
+        ctx.drawImage(img, (i-8)*158+15, 587, 128, 128)
     }
 }
 
